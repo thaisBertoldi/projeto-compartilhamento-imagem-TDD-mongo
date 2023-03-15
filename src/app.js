ea29b8962ab54ee1fa6abf3ecdeec08662bde91e
Thais B <thais_brt@hotmail.com>
2023-03-15 17:45:11 -0300

    feat: conexao com o mongodb

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 let express = require('express');
 let app = express();
+let mongoose = require('mongoose');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+mongoose.connect('mongodb://localhost:27017/guiapics');
 
 app.get('/', (req, res) => {
     res.json({});
