72b897527ccc03b8c0b3b50809e34995282dd31e
thaisBertoldi <thais_brt@hotmail.com>
2023-04-08 19:02:10 -0300

    feat: create user and test

--- src/app.js
+++ src/app.js
@@ -1,17 +1,35 @@
 let express = require('express');
 let app = express();
 let mongoose = require('mongoose');
+let userModel = require('./models/User');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
 mongoose.connect('mongodb://localhost:27017/guiapics')
 .then(() => {})
 .catch((err) => {
     console.log(err);
 })
 
+let User = mongoose.model('User', userModel);
+
 app.get('/', (req, res) => {
     res.json({});
 });
+
+app.post('/user', async (req, res) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try {
+        await newUser.save();
+        res.json({ email: req.body.email });
+    } catch (error) {
+        res.sendStatus(500);
+    }
+})
 
 module.exports = app;
